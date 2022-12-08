0078a98f0950ae6905a764cfb33dd4c828fdc741
Stephanie Miller <millerbee.steph@gmail.com>
2022-12-07 22:25:02 -0700

    added list functionaliy and work todo page

--- app.js
+++ app.js
@@ -1,13 +1,46 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/views/date.js");
+const day = require(__dirname + "/views/date.js"); //not used in code. day variable and associated function is purely for example purposes only
 
 const app = express();
 
+const items = [];
+const workItems = [];
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
 app.get("/", function(req, res) {
-  res.send("hello");
+  
+  const day = date.getDate();
+
+  res.render("list", {listTitle: day, newToDoItems: items});
+
 });
 
+app.post("/", function(req, res) {
 
+  const item = req.body.todoItem;
+  
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+});
+
+app.get("/work", function(req, res) {
+  res.render("list", {listTitle: "Work List", newToDoItems: workItems});
+});
+
+app.get("/about", function(req, res) {
+  res.render("about");
+});
 
 
 app.listen(3000, function() {
